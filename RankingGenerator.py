958a54603feeeb7c0d77d393367db9a90477361f
Mischa <35734847+dmb23@users.noreply.github.com>
2018-01-23 16:20:37 +0100

    Added Comments

--- RankingGenerator.py
+++ RankingGenerator.py
@@ -4,6 +4,7 @@ import sys
 import matplotlib.pyplot as plt
 from typing import List
 
+# conversion table: difference in rank to expected result of matchup
 rankDeltaToResultDelta = [
     0,
     1,
@@ -28,12 +29,19 @@ rankDeltaToResultDelta = [
 
 
 def swap(list: List[str], indexA: int, indexB: int):
+    """ swap two list members
+    """
     temp = list[indexA]
     list[indexA] = list[indexB]
     list[indexB] = temp
 
 
 def calculateTotalError(currentRanking: List[str], results: List[Result]) -> float:
+    """ Error between obtained results and results expected from ranking
+    Takes into account all obtained results so far
+    Converts given Ranking into results using conversion table
+    Calculates quadratic error
+    """
     lossSum = 0
     for result in results:
         rankA = currentRanking.index(result.matchUp.first)
@@ -48,32 +56,57 @@ def calculateTotalError(currentRanking: List[str], results: List[Result]) -> flo
 
 
 def generateNewRanking(currentRanking: List[str], results: List[Result], debug=False) -> List[str]:
+    """ heuristic search for an optimal Ranking to explain the results
+    compares random permutations in their total error
+    uses some variant of simulated annealing for the search
+    XXX possible time gain: better determination criteria, Tabu-List
+    XXX pAcceptWorse=4.5e-5 after 1e5 iterations, currently 1e6 iterations are set
+    """
+    # list of errors made by different rankings
     losses = []
+    # current elements in the search
     minimalLoss = sys.maxsize
     minimalLossRanking = currentRanking[:]
     newRanking = currentRanking[:]
-    decay = 0.9999
+    # tuning parameters for search
     pAcceptWorse = 0.5 # probability to accept a worse result
+    decay = 0.9999     # decay parameter for pAcceptWorse
+    if debug:
+        print("Original ranking:")
+        for item in currentRanking:
+            print(item)
+        print("initial Erorr: loss=", calculateTotalError(currentRanking, results))
+    # cover large range of possible random permutations:
     for i in range(0,1000000):
+        # calculate total Error made by current ranking
         currentLoss = calculateTotalError(newRanking, results)
         losses.append(currentLoss)
+        # randomly permute the ranking
         indexA = random.randint(0, len(currentRanking)-1)
         indexB = indexA
         while indexA == indexB:
             indexB = random.randint(0, len(currentRanking)-1)
         swap(newRanking, indexA, indexB)
+        # calculate total error of new ranking
         newLoss = calculateTotalError(newRanking, results)
-        if newLoss < minimalLoss: # save optimal solution
+        # if better: save optimal solution
+        if newLoss < minimalLoss: 
             minimalLoss = newLoss
             minimalLossRanking = newRanking[:]
+        # possibly use worse ranking (to avoid local minima)
         if newLoss > currentLoss and random.uniform(0.0, 1.0) > pAcceptWorse: # do not accept worse value
             swap(newRanking, indexA, indexB)
         pAcceptWorse *= decay
-    if debug:
+    if debug: # debug messaging
+        print("Optimal Ranking:")
         for item in minimalLossRanking:
             print(item)
-        print("loss", minimalLoss)
+        print("Optimal Error: loss=", minimalLoss)
         print("pAcceptWorse", pAcceptWorse)
-        plt.plot(losses)
+        fig1,ax1=plt.subplots()
+        ax1.plot(losses)
+        ax1.set_xlabel('iteration #')
+        ax1.set_ylabel('total error')
+        ax1.set_title('Error for different permutations')
         plt.show()
     return minimalLossRanking
--- SwissGameScheduler.py
+++ SwissGameScheduler.py
@@ -5,6 +5,8 @@ from typing import List
 
 class SwissGameScheduler:
     def getTimeDelta(self, previousMatches: List[Match], teamName: str, futureSlot: Slot) -> int:
+        """ calculate pause for teamName between last game and futureSlot
+        """
         delta = sys.maxsize
         for match in previousMatches:
             if match.matchUp.first == teamName or match.matchUp.second == teamName:
@@ -13,6 +15,8 @@ class SwissGameScheduler:
 
 
     def getTimesPlayedInHall3(self, previousMatches: List[Match], futureMatchUp: MatchUp) -> int:
+        """ calculate max(# games played in hall 3) for two teams in futureMatchUp
+        """
         numHall3GamesA = 0
         numHall3GamesB = 0
         for match in previousMatches:
@@ -27,6 +31,9 @@ class SwissGameScheduler:
 
     #TODO this only if all games happen on the same day! #FIXME
     def getHallChangeNeeded(self, previousMatches: List[Match], futureMatchUp: MatchUp, futureSlot: Slot) -> bool:
+        """ calculate if a team needs to change the gym for futureMatchUp
+        Searches for last games, ignores day of the weekend.
+        """
         # case 1: hall 1 or hall 2
         # case 3: hall 3
         mostRecentHallA = 0
@@ -61,29 +68,48 @@ class SwissGameScheduler:
 
 
     def calculateGain(self, previousMatches: List[Match], futureMatchUp: MatchUp, futureSlot: Slot) -> float:
+        """ calculate 'Gain' for futureMatchUp in futureSlot
+        'Gain' is some value of a future matchup:
+        high values are good, low (and negative) values are bad
+        """
+        # tuning parameters
         hall3TransferPenalty = 30 # minutes
         targetDelta = 100 # how much time optimally should be between games
+        ##
 
         timesPlayedInHall3 = self.getTimesPlayedInHall3(previousMatches, futureMatchUp)
         hallChangeNeeded = self.getHallChangeNeeded(previousMatches, futureMatchUp, futureSlot)
+        ##
 
+        # calculate pauses between games for both teams
         firstDelta = self.getTimeDelta(previousMatches, futureMatchUp.first, futureSlot)
         secondDelta = self.getTimeDelta(previousMatches, futureMatchUp.second, futureSlot)
+        # subtract optimal pause length from effective pause length
         firstCorrectedDelta = firstDelta - targetDelta
         secondCorrectedDelta = secondDelta- targetDelta
+        # take smaller difference (punish small pause)
         gainSource = min(firstCorrectedDelta, secondCorrectedDelta)
         if hallChangeNeeded:
+            # further subtract walking time
             gainSource -= hall3TransferPenalty
 
+        # acceptable pause length:
         if gainSource >= 0:
+
+            # penalize multiple games of a certain team in Gym 3
             if timesPlayedInHall3 > 1 and futureSlot.locationId == 3:
                 return 20 / timesPlayedInHall3
             else:
                 return 20
+        # too short pause lenght
         else:
             return -(gainSource**2) # heavily penalize for being above targetDelta threshold
 
     def genGainMatrix(self, previousMatches: List[Match], futureSlots: List[Slot], futureMatchUps: List[MatchUp]) -> List[List[float]]:
+        """ calculate matrix of gain for possible matches
+        distributes future matchups onto future slots,
+        calculates all possible gains
+        """
         assert len(previousMatches) > 0 and len(futureSlots) > 0 and len(futureMatchUps) > 0
 
         gainMatrix = []
@@ -96,6 +122,8 @@ class SwissGameScheduler:
 
 
     def getGainSum(self, gainMatrix: List[List[float]], matchUpIndexList: List[int]) -> float:
+        """ calculate sum(gain) for given realization of matchups onto futureslots
+        """
         gainSum = 0
         for slotIndex in range(0, len(matchUpIndexList)):
             gainSum += gainMatrix[slotIndex][matchUpIndexList[slotIndex]]
@@ -103,23 +131,31 @@ class SwissGameScheduler:
 
 
     def maximizeGain(self, previousMatches: List[Match], futureSlots: List[Slot], futureMatchUps: List[MatchUp]) -> None:
-        #TODO assert correct size
+        """ calculate optimal schedule for given futureMatchUps in given futureSlots
+        maximize 'Gain' measure for distribution of matchups onto slots
+        """
+        #TODO BART: assert correct size
         gainMatrix = self.genGainMatrix(previousMatches, futureSlots, futureMatchUps)
         swapped = True
         matchupIndexList = list(range(0, len(gainMatrix)))
+        # search optimal permutation by swapping matchups on their slots
         while swapped:
             swapped = False
+            # go through all possible matchups in list
             for offset in range(0, len(matchupIndexList) - 1):
+                # swap them with matchups later in the list
                 for i in range(offset + 1, len(matchupIndexList)):
                     currentGainSum = self.getGainSum(gainMatrix, matchupIndexList)
                     newMatchUpIndexList = matchupIndexList[:]
                     newMatchUpIndexList[i] = matchupIndexList[offset]
                     newMatchUpIndexList[offset] = matchupIndexList[i]
                     newGainSum = self.getGainSum(gainMatrix, newMatchUpIndexList)
+                    # if 'Gain' is higher, repeat search
                     if(newGainSum > currentGainSum):
                         swapped = True
                         matchupIndexList = newMatchUpIndexList[:]
                         print(currentGainSum)
+        # print final schedule
         for i in range(0, len(matchupIndexList)):
             print("start {} end {} (hall {})  {} : {}".format(self.printTime(futureSlots[i].start),
                                                               self.printTime(futureSlots[i].end),
@@ -133,5 +169,7 @@ class SwissGameScheduler:
 
 
     def printTime(self, minutes: int) -> str:
+        """ print time(#min from 00:00) in hh:mm
+        """
         return "{:02d}:{:02d}".format(minutes // 60, minutes % 60)
 
