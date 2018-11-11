a0f01fe1e7a9ea8299885784e6e7dfb5548cf4ed
Jan-Bernd <bart@jan-bart.de>
2018-11-11 14:10:38 +0100

    integrate new cpp ranking generator seamlessly into python stuff

--- RankingGenerator.py
+++ RankingGenerator.py
@@ -55,7 +55,7 @@ def _calculateTotalError(currentRanking: List[str], games: List[Game]) -> float:
     return lossSum
 
 
-def generateNewRanking(currentRanking: List[str], games: List[Game], debug=False) -> List[str]:
+def generateNewRankingPython(currentRanking: List[str], games: List[Game], debug=False) -> List[str]:
     """ heuristic search for an optimal Ranking to explain the results
     compares random permutations in their total error
     uses some variant of simulated annealing for the search
@@ -110,3 +110,40 @@ def generateNewRanking(currentRanking: List[str], games: List[Game], debug=False
         ax1.set_title('Error for different permutations')
         plt.show()
     return minimalLossRanking
+
+def generateNewRankingCpp(currentRanking: List[str], games: List[Game], debug=False) -> List[str]:
+    teamNameToInt = dict()
+    for number, teamName in enumerate(currentRanking, 0):
+        teamNameToInt[teamName] = number
+
+    currentRankingInt = list()
+    for teamName in currentRanking:
+        currentRankingInt.append(teamNameToInt[teamName])
+
+    gamesInt = list()
+    for game in games:
+        gameInt = RankingGeneratorCpp.GameInt(teamNameToInt[game.matchup.first],
+                                    teamNameToInt[game.matchup.second],
+                                    game.result.first,
+                                    game.result.second)
+        gamesInt.append(gameInt)
+
+    rankingGeneratorCpp = RankingGeneratorCpp.RankingGenerator()
+    rankingInt = rankingGeneratorCpp.generateRanking(currentRankingInt, gamesInt, debug)
+
+    newRanking = list()
+    for number in rankingInt:
+        for key, value in teamNameToInt:
+            if value == number:
+                newRanking.append(key)
+                break
+    return newRanking
+
+try:
+    import RankingGeneratorCpp
+    rankingFunction = generateNewRankingCpp
+except ImportError:
+    rankingFunction = generateNewRankingPython
+
+def generateNewRanking(currentRanking: List[str], games: List[Game], debug=False) -> List[str]:
+    return rankingFunction(currentRanking, games, debug)
