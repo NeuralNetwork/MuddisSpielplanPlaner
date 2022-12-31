dbdcb5259609635f8bee3ee23fad95118bd25a4a
Jan-Bernd Vosteen <bart@jan-bart.de>
2022-12-31 21:51:35 +0100

    added debug code to main

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
+#include <atomic>
+#include <thread>
 #include "RankingGenerator.h"
 
 int main() {
-  const bool debug = true;
-  std::vector<int> currentRanking = {
-          0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17
-  };
+  const bool debug = false;
+  std::vector<TeamInt> currentRanking = {
+          {15,16},
+          {14,15},
+          {13,14},
+          {12,13},
+          {11,12},
+          {10,11},
+          {9, 10},
+          {8,9},
+          {7, 8},
+          {6, 7},
+          {5, 6},
+          {4, 5},
+          {3, 4},
+          {2, 3},
+          {1, 2},
+          {0, 1}};
   std::vector<GameInt> games = {
           GameInt(0,1,6,11),
           GameInt(2,3,12,5),
@@ -23,6 +39,44 @@ int main() {
           GameInt(14,15,13,3),
           GameInt(16,17,10,6),
   };
-  RankingGenerator rankingGenerator;
-  rankingGenerator.generateRanking(currentRanking, games, debug);
+  std::vector<GameInt> neutralGames = {
+          {0,1,11,10},
+          {2,3,11,10},
+          {4,5,11,10},
+          {6,7,11,10},
+          {8,9,11,10},
+          {10,11,11,10},
+          {12,13,11,10},
+          {14,15,11,10},
+          {1,2,11,10},
+          {3,4,11,10},
+          {5,6,11,10},
+          {7,8,11,10},
+          {9,10,11,10},
+          {11,12,11,10},
+          {13,14,11,10}
+  };
+
+  std::atomic_int32_t notNullCount;
+  notNullCount = 0;
+  auto evalFunc = [&] () {
+    for(int i = 0; i < 1000; ++i) {
+      RankingGenerator rankingGenerator;
+      auto newRanking = rankingGenerator.generateRanking(currentRanking, neutralGames, debug);
+      if(rankingGenerator.calculateTotalError(newRanking, neutralGames))
+        notNullCount++;
+    }
+  };
+
+  std::vector<std::thread> threads;
+  for(int i = 0; i < 8; ++i) {
+    std::thread oneThread(evalFunc);
+    threads.push_back(std::move(oneThread));
+  }
+  for(auto& thread : threads)
+  {
+    thread.join();
+  }
+
+  std::cout << notNullCount << std::endl;
 }
